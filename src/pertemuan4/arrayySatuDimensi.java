6cc8f9a2d0b9fc1125cf38e6a752cee0817439b9
vitrinurch <67619932+vitrinurch@users.noreply.github.com>
2020-08-03 20:07:14 +0700

    Add files via upload

---
+++ src/pertemuan4/arrayySatuDimensi.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajar.arrayy;
+
+/**
+ *
+ * @author HP 14-AM514TU
+ */
+public class arrayySatuDimensi {
+    public static void main(String[] args) {
+        
+        String kotakmakeup[] = {"Televisi", "Mesin Cuci", "Kulkas",};
+        
+        System.out.println("Cek kotakmakeup : "+kotakmakeup[2]);
+        System.out.println("Cek kotakmakeup : "+kotakmakeup[2]);
+        System.out.println("Cek kotakmakeup : "+kotakmakeup[2]);
+    }
+}
